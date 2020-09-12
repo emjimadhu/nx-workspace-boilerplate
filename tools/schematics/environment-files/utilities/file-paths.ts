7bf002978f812f17d0aea36d7265ca2e9fcf5c65
Em Ji Madhu <emjimadhu@gmail.com>
2020-09-12 13:56:37 +0530

    chore(*): :fire: remove workspace schematics

--- tools/schematics/environment-files/utilities/file-paths.ts
+++
@@ -1,4 +0,0 @@
-export const initialSetupFilesPath = './files/init';
-export const outputDirectoryLocation = './project-tools/environments';
-export const outputSharedDirectoryLocation = `${outputDirectoryLocation}/shared`;
-export const appFilePath = './files/app';
